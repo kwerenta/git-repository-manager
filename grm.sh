ee7e28431fedcb4ae6f1b208cb3e96d6e1c6b674
kwerenta <kamil.wenta222@gmail.com>
2023-05-10 21:25:34 +0200

    Add header

--- grm.sh
+++ grm.sh
@@ -1,2 +1,11 @@
 #!/bin/bash
+# Name             : Git Repository Manager
+# Author           : Kamil Wenta (193437)
+# Created On       : 10.05.2023r.
+# Last Modified By : Kamil Wenta (193437)
+# Last Modified On : 10.05.2023r. 
+# Version          : 0.1.0
+#
+# Description      :
+# GUI to manage git repositories and more
 echo "Hello world"
